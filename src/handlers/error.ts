697a67894ce05bc9946c5d51aec55ddb9c47d446
Norberto Caceres <norberto.e.888@gmail.com>
2020-09-25 12:24:02 -0500

    [ADD] Log error in handlers/error

--- src/handlers/error.ts
+++ src/handlers/error.ts
@@ -1,8 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
 import { normalizeResponse } from '../util'
 
-export default (error: any, _: Request, res: Response, __: NextFunction) =>
-	res.status(error.statusCode || 500).json(
+export default (error: any, _: Request, res: Response, __: NextFunction) => {
+	console.error(error)
+	return res.status(error.statusCode || 500).json(
 		normalizeResponse({
 			status: error.statusCode || 500,
 			isError: true,
@@ -10,3 +11,4 @@ export default (error: any, _: Request, res: Response, __: NextFunction) =>
 			message: error.error || error.message || 'Something went wrong.',
 		})
 	)
+}
