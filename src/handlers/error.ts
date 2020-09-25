a4d8ec2b863c8b98cd7c2824ff6fdd4702f9fb65
Norberto Caceres <norberto.e.888@gmail.com>
2020-09-25 12:46:58 -0500

    [FIX]

--- src/handlers/error.ts
+++ src/handlers/error.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
 import { normalizeResponse } from '../util'
 
-export default (error: any, _: Request, res: Response, __: NextFunction) => {
-	console.error(error)
-	return res.status(error.statusCode || 500).json(
+export default (error: any, _: Request, res: Response, __: NextFunction) =>
+	res.status(error.statusCode || 500).json(
 		normalizeResponse({
 			status: error.statusCode || 500,
 			isError: true,
@@ -11,4 +10,3 @@ export default (error: any, _: Request, res: Response, __: NextFunction) => {
 			message: error.error || error.message || 'Something went wrong.',
 		})
 	)
-}
