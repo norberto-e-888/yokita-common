eb3320f4fd2f84c795d6eb1bef0084a8b3ece8f6
Norberto Caceres <norberto.e.888@gmail.com>
2020-10-18 13:13:20 -0500

    Updates

--- src/nats/types.ts
+++ src/nats/types.ts
@@ -1,9 +1,11 @@
 export enum Subject {
 	TicketCreated = 'ticket:created',
 	TicketUpdated = 'ticket:updated',
+	OrderCreated = 'order:created',
+	OrderCancelled = 'order:cancelled',
 }
 
-export interface Event {
+export interface Event<S, D> {
 	subject: Subject
 	data: any
 }
@@ -27,3 +29,32 @@ export interface TicketUpdatedEvent {
 		userId: string
 	}
 }
+
+export interface OrderCreatedEvent {
+	subject: Subject.OrderCreated
+	data: {
+		id: string
+		status: any
+		userId: string
+		expiresAt: string
+		ticket: {
+			id: string
+			price: number
+		}
+	}
+}
+
+export interface OrderCancelledEvent
+	extends Event<
+		Subject.OrderCancelled,
+		{
+			id: string
+			status: any
+			userId: string
+			expiresAt: string
+			ticket: {
+				id: string
+				price: number
+			}
+		}
+	> {}
