19151f3a750270b0e8e274c8b01aee2f65d010da
Norberto Caceres <norberto.e.888@gmail.com>
2020-10-19 16:51:27 -0500

    [UPDATE]

--- src/nats/subscriber.ts
+++ src/nats/subscriber.ts
@@ -21,8 +21,6 @@ export default abstract class Subscriber<T extends Event<Subject, any>> {
 	}
 
 	listen(subject: Subject) {
-		console.log('subject', subject)
-		console.log('this.queueGroupName', this.queueGroupName)
 		const subscription = this.stan.subscribe(
 			subject,
 			this.queueGroupName,
