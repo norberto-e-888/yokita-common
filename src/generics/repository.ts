2261a0bd5c5fea81255f98c50a259f232bd9697e
Norberto Caceres <norberto.e.888@gmail.com>
2020-11-05 17:28:14 -0500

    [DEBUG]

--- src/generics/repository.ts
+++ src/generics/repository.ts
@@ -42,7 +42,7 @@ export default class GenericRepository<D extends Document, O = any> {
 	): Promise<FetchingResult<D>> {
 		console.log('QUERY', query)
 		const fetchPipelineBuilder = new FetchPipelineBuilder(query, options)
-		console.log('PIPELINE', fetchPipelineBuilder.pipeline)
+		console.log('PIPELINE', JSON.stringify(fetchPipelineBuilder.pipeline))
 		const [data] = ((await this.model.aggregate(
 			fetchPipelineBuilder.pipeline
 		)) as unknown) as [FetchingResult<D>]
