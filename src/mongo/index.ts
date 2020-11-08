e115e71eaf47c52120a0c94813b282aebe1313fe
Norberto Caceres <norberto.e.888@gmail.com>
2020-11-08 17:34:03 -0500

    Updates

--- src/mongo/index.ts
+++ src/mongo/index.ts
@@ -1,25 +1,24 @@
 import { Document, HookNextFunction, UpdateQuery } from 'mongoose'
 
-export async function handlePreFindOneAndUpdateVersionIncrement(
-	this: UpdateQuery<Document>,
-	next: HookNextFunction
-) {
-	const update = this.getUpdate()
-	if (update.version !== null) {
-		delete update.version
-	}
+export function handlePreFindOneAndUpdateVersionIncrement(versionKey: string) {
+	return function (this: UpdateQuery<Document>, next: HookNextFunction) {
+		const update = this.getUpdate()
+		if (update[versionKey] !== null) {
+			delete update[versionKey]
+		}
 
-	const keys = ['$set', '$setOnInsert']
-	for (const key of keys) {
-		if (update[key] !== null && update[key].version !== null) {
-			delete update[key].version
-			if (Object.keys(update[key]).length === 0) {
-				delete update[key]
+		const keys = ['$set', '$setOnInsert']
+		for (const key of keys) {
+			if (update[key] !== null && update[key][versionKey] !== null) {
+				delete update[key][versionKey]
+				if (Object.keys(update[key]).length === 0) {
+					delete update[key]
+				}
 			}
 		}
-	}
 
-	update.$inc = update.$inc || {}
-	update.$inc.version = 1
-	next()
+		update.$inc = update.$inc || {}
+		update.$inc[versionKey] = 1
+		next()
+	}
 }
