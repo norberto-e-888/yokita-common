95e3a8d9ff62c8b8da677a35354497eebe0fc65a
Norberto Cáceres <norberto.e.888@outlook.es>
2020-11-30 12:47:20 -0500

    Updates

--- src/generics/index.ts
+++ src/generics/index.ts
@@ -2,8 +2,3 @@ export { default as GenericController } from './controller'
 export * from './nats-replica-subscriber'
 export { default as PlainRepository } from './plain-repository'
 export * from './plain-repository'
-export {
-	default as GenericRepository,
-	FindByIdOptions,
-	GenericRepositoryConstructorOptions,
-} from './repository'
